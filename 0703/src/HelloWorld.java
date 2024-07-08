5cc9de9480ddf0a43a565021e29060f3a65a5583
Hong <honginpyo2100@gmail.com>
2024-07-08 16:39:46 +0900

    [INIT] 매일 작성한 자바소스

---
+++ 0703/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String [] args){
+		String msg = "Hello, 자바~";
+		System.out.printf("msg = %s\n" , msg);
+	}
+
+}
---
+++ 0704/src/VariableDemo.java
@@ -0,0 +1,32 @@
+
+public class VariableDemo {
+	public static void main(String[] args) {
+		String name; //변수선언 Variable 
+		name = "홍인표" // 할당 Assignment
+		String email = "hip1hip@naver.com" //초기화 
+		
+		System.out.printf("숫자 5 = %d\n", 500);
+		System.out.printf("숫자 5 = %o\n", 500);
+		System.out.printf("숫자 5 = %x\n", 500);
+		
+		float f = 0.123456789123456789f;
+		double d = 0.1234567890123456789;
+		
+		
+		System.out.print('a');
+		System.out.print('b');
+		System.out.print('\n');
+		System.out.print('c');
+		System.out.print('d');
+		System.out.print('\b');
+		System.out.print('e');
+		
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println(4 != 5);
+		
+
+		
+		
+	}
+}
