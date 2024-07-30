2e21da41ff6f93dfe1660de1983a151c1e620d46
Hong <honginpyo2100@gmail.com>
2024-07-30 17:20:44 +0900

    07월 30일 소스코드

---
+++ PatientManagement/patient.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Patient(
+	number TINYINT NOT NULL,
+	code CHAR(2) NOT NULL,
+	days SMALLINT NOT NULL, 
+	age TINYINT NOT NULL ,
+	dept VARCHAR(20) , 
+	operfee INT ,
+	hospitalfee INT ,
+	money  INT , 
+	CONSTRAINT patient_number_pk PRIMARY KEY(number)
+	
+);
--- PatientManagement/src/com/example/main/Main.java
+++ PatientManagement/src/com/example/main/Main.java
@@ -2,7 +2,7 @@ package com.example.main;
 import com.example.view.PatientView;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		new PatientView();
 	}
 }
