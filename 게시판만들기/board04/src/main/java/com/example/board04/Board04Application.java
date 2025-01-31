b1e6869ac9497f87849f152478deee7665871fae
hip1hip <honginpyo2100@gmail.com>
2025-01-31 11:04:37 +0900

    feat: board04 개발 시작

---
+++ 게시판만들기/board04/src/main/java/com/example/board04/Board04Application.java
@@ -0,0 +1,13 @@
+package com.example.board04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Board04Application.class, args);
+    }
+
+}
---
+++ 게시판만들기/board04/src/main/java/com/example/board04/dto/Board.java
@@ -0,0 +1,5 @@
+package com.example.board04.dto;
+
+public class Board {
+    
+}
